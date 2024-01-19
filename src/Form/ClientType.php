8a9ef9cf9b4fc8fafad2ffe5f0f09f3dcc020524
Alexey Tretyakov <ralfzeit@gmail.com>
2024-01-19 18:50:57 +0700

    Русифицирована форма редактирования клиента

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -6,23 +6,36 @@ use App\Entity\Client;
 use App\Entity\Education;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ClientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('phone')
-            ->add('email')
+            ->add('name', TextType::class, [
+                'label' => 'Имя',
+            ])
+            ->add('surname', TextType::class, [
+                'label' => 'Фамилия',
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Номер телефона',
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+            ])
             ->add('education_id', EntityType::class, [
                 'class' => Education::class,
-                'choice_label' => 'id',
+                'choice_label' => 'title',
+                'label' => 'Образование',
             ])
-            ->add('agree')
+            ->add('agree', CheckboxType::class, [
+                'label' => 'Email',
+            ])
         ;
     }
 
