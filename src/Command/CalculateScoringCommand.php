268c999e9e69d4201660ca339536f5a6234984cf
Alexey Tretyakov <ralfzeit@gmail.com>
2024-01-22 11:57:54 +0700

    Консольная команда для расчета скоринга (без записи в БД)

--- src/Command/CalculateScoringCommand.php
+++ src/Command/CalculateScoringCommand.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Command;
-namespace App\Service\ScoringService;
 
+use App\Service\ScoringService;
+use App\Entity\Client;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,6 +19,17 @@ use Symfony\Component\Console\Input\InputArgument;
 )]
 class CalculateScoringCommand extends Command
 {
+    private $scoringService;
+    private $doctrine;
+
+    public function __construct(ScoringService $scoringService, ManagerRegistry $doctrine)
+    {
+        $this->scoringService = $scoringService;
+        $this->doctrine = $doctrine;
+
+        parent::__construct();
+    }
+    
     protected function configure(): void
     {
         // сообщение помощи команды, отображаемое при запуске команды с опцией "--help"
@@ -30,11 +43,37 @@ class CalculateScoringCommand extends Command
         $id = $input->getArgument('id');
 
         //Расчет скоринга для всех клиентов
-        if(!$id) 
+        if(!$id){
             $output->writeln('<fg=green>Расчет скоринга для всех клиентов</>');
+            
+            $clients = $this->doctrine->getRepository(Client::class)->findAll();
+
+            foreach($clients as $client){
+                $details = $this->scoringService->calculateScoreForConsole($client->getPhone(), $client->getEmail(), $client->getEducationId()->getTitle(), $client->isAgree());
+                $output->writeln('ID клиента: '.$client->getId());
+                $output->writeln('Сотовый оператор: '.$details["provider"]);
+                $output->writeln('Домен почты: '.$details["domain"]);
+                $output->writeln('Образование: '.$details["education"]);
+                $output->writeln('Согласие: '.$details["agree"]);
+                $output->writeln('Актуальный скоринг в БД: '.$client->getScore());
+                $output->writeln('<fg=blue>Скоринг (сумма баллов): '.(string)$details["scoring"].'</>');
+                $output->writeln('');
+            }
+        }
         //Расчет скоринга для одного клиента
-        else if ((is_int($id) || ctype_digit($id)) && (int)$id >= 0 )
-            $output->writeln('<fg=blue>Расчет скоринга для клиента с id='.$input->getArgument('id'));
+        else if ((is_int($id) || ctype_digit($id)) && (int)$id >= 0 ){
+            $output->writeln('<fg=green>Расчет скоринга с детализацией для клиента с id='.$input->getArgument('id').'</>');
+            
+            $client = $this->doctrine->getRepository(Client::class)->find($id);
+            $details = $this->scoringService->calculateScoreForConsole($client->getPhone(), $client->getEmail(), $client->getEducationId()->getTitle(), $client->isAgree());
+            $output->writeln('Сотовый оператор: '.$details["provider"]);
+            $output->writeln('Домен почты: '.$details["domain"]);
+            $output->writeln('Образование: '.$details["education"]);
+            $output->writeln('Согласие: '.$details["agree"]);
+            $output->writeln('Актуальный скоринг в БД: '.$client->getScore());
+            $output->writeln('<fg=blue>Скоринг (сумма баллов): '.(string)$details["scoring"].'</>');
+            $output->writeln('');
+        }
         //Если введен неправильный аргумент
         else {
             $output->writeln('<fg=red>Введено некорректное значение id</>');
--- src/Service/ScoringService.php
+++ src/Service/ScoringService.php
@@ -4,54 +4,92 @@ namespace App\Service;
 
 class ScoringService
 {
-    public function calculateScoreReg(String $phone, String $email, String $education, bool $agree): string
+    public function getProdiverScore(String $phone): int
     {
         $providerCode = (int)substr($phone, 1, 3);
-        $mailDomain = substr($email, 0, strripos($email, '.')); 
-        $mailDomain = substr($mailDomain, strripos($mailDomain, '@')+1); 
-
-        //Количество баллов за иного оператора
-        $providerScore = 1;
-        //Количество баллов за среднее образование
-        $educationScore = 5;
-        //Количество баллов за отсутствие галочки
-        $agreeScore = 0;
-        //Количество баллов за иной домен
-        $domainScore = 3;
 
         //Если МТС
         if (($providerCode >= 910 && $providerCode <= 919) || 
             ($providerCode >= 980 && $providerCode <= 989))
-                $providerScore = 3;
+                return 3;
         //Если Билайн
         else if (($providerCode >= 903 && $providerCode <= 906) || 
                     ($providerCode >= 960 && $providerCode <= 968))
-                        $providerScore = 5;
+                        return 5;
         //Если Мегафон
         else if ($providerCode >= 920 && $providerCode <= 938)
-                    $providerScore = 10;
+                    return 10;
+        //Если иной оператор
+        else return 1;
+    }
 
+    public function getEducationScore(String $education): int
+    {
         //Если специальное
         if (strcasecmp($education, 'Специальное образование') == 0)
-            $educationScore = 10;
+            return 10;
         //Если высшее
         else if (strcasecmp($education, 'Высшее образование') == 0)
-            $educationScore = 15;
+            return 15;
+        //Если среднее
+        else return 5;
+    }
+
+    public function getAgreeScore(bool $agree): int
+    {
+        //Если галочка установлена 
+        if ($agree)
+            return 4;
+        //Если не установлена
+        else 
+            return 0;
+    }
+
+    public function getDomainScore(String $email): int 
+    {
+        $mailDomain = substr($email, 0, strripos($email, '.')); 
+        $mailDomain = substr($mailDomain, strripos($mailDomain, '@')+1); 
 
         //Если mail
         if (strcasecmp($mailDomain, 'mail') == 0)
-            $domainScore = 6;
+            return 6;
         //Если yandex
         else if (strcasecmp($mailDomain, 'yandex') == 0)
-            $domainScore = 8;
+            return 8;
         //Если gmail
         else if (strcasecmp($mailDomain, 'gmail') == 0)
-            $domainScore = 10;
+            return 10;
+        //иной
+        else return 3;
+    }
 
-        //Если галочка установлена 
-        if ($agree)
-            $agreeScore = 4;
+    public function calculateScoreReg(String $phone, String $email, String $education, bool $agree): string
+    {
+        //Количество баллов за оператора
+        $providerScore = $this->getProdiverScore($phone);
+
+        //Количество баллов за образование
+        $educationScore = $this->getEducationScore($education);
+        
+        //Количество баллов за галочку
+        $agreeScore = $this->getAgreeScore($agree);
+        
+        //Количество баллов за домен
+        $domainScore = $this->getDomainScore($email);
 
         return $providerScore + $domainScore + $agreeScore + $educationScore;
+    }
+
+    public function calculateScoreForConsole(String $phone, String $email, String $education, bool $agree): array
+    {
+        $details = array(
+            "provider" => $this->getProdiverScore($phone),
+            "domain" => $this->getDomainScore($email),
+            "education" => $this->getEducationScore($education),
+            "agree" => $this->getAgreeScore($agree),
+            "scoring" => $this->calculateScoreReg($phone, $email, $education, $agree),
+        );
+
+        return $details;
     }
 }
